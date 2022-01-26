9b9c816c1fdc38121aa076e6bc00e656d74d11b8
SebastianRuiz18 <89488090+SebastianRuiz18@users.noreply.github.com>
2022-01-25 18:25:25 -0800

    Add files via upload

---
+++ 103/Session#1/script.js
@@ -0,0 +1,87 @@
+// This is a JS comment
+    console.log("Script");
+    var client;
+    client = "Samantha";
+    let age = 99;
+    const URL = "http://127.0.0.1/iot";
+    age = 55;
+
+    console.log(age);
+    console.log(URL);
+
+    client = "Maira";
+
+    client = "Aaron";
+
+    console.log("client"); // Client
+    console.log(client);// Prints the client variable
+
+    console.log("Please, " + client + " Visit the site: " + URL)
+
+    // Exercise #1
+
+    // Declare
+    var jobTitle;
+    var geoLocation;
+    var partnerName;
+    var numberChildren;
+
+    // Assign
+    jobTitle = "JOB";
+    geoLocation = "LOCATION";
+    partnerName = "PARTNER";
+    numberChildren = 10;
+
+    // Output/Print
+    //console.log("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partnerName + " with " + numberChildren + " kids.");
+
+    //document.write(`Please, ${client} visit the site: ${URL}\n `);
+
+    // Assignment 1 (Display 20 variables)
+
+    // Declare
+    var userID;
+    var userName;
+    var userAddress;
+    var userEmail;
+    var userPassword;
+    var userAge;
+    var userNationality;
+    var userGender;
+    var userMarried;
+    var favColor;
+    var favSong;
+    var favMovie;
+    var favSport;
+    var favBand;
+    var favTeam;
+    var userEducation;
+    var userInstitution;
+    var userHeightmet;
+    var userWeightkg;
+    var userWebpage;
+    
+    // Assign
+    userID = 1;
+    userName = "User123";
+    userAddress = "1674 Random St, City Valley, CA, USA";
+    userEmail = "User123@gmail.com";
+    userPassword = "P4$$word";
+    userAge = 21;
+    userNationality = "Egiptian";
+    userGender = "Male";
+    userMarried = false;
+    favColor = "Red";
+    favSong = "Breed";
+    favMovie = "Back to the future";
+    favSport = "Football (Soccer)";
+    favBand = "Nirvana";
+    favTeam = "Arsenal FC";
+    userEducation = "University";
+    userInstitution = "MIT";
+    userHeightmet = 2.07;
+    userWeightkg = 80;
+    userWebpage = "https://www.userexameple.com"
+
+    // Print
+    document.write(`ID: ${userID}<br/>Username: ${userName}<br/>Adress: ${userAddress}<br/>E-mail: ${userEmail}<br/>Password: ${userPassword}<br/>Age: ${userAge}<br/>Nationality: ${userNationality}<br/>Gender: ${userGender}<br/>Married: ${userMarried}<br/>Favorite Color: ${favColor}<br/>Favorite Song: ${favSong}<br/>Favorite Movie: ${favMovie}<br/>Favorite Sport: ${favSport}<br/>Favorite Band: ${favBand}<br/>Favorite Team: ${favTeam}<br/>Education: ${userEducation}<br/>Institution: ${userInstitution}<br/>Heigth (Meters): ${userHeightmet}m <br/>Weight (Kilograms)${userWeightkg}Kg <br/>Webpage: ${userWebpage}`)
