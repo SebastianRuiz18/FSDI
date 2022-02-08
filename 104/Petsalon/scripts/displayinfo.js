3210e2864b2e334dad7b12fc104d9876eda8d0d5
SebastianRuiz18 <89488090+SebastianRuiz18@users.noreply.github.com>
2022-02-07 20:05:17 -0800

    Add files via upload

--- 104/Petsalon/scripts/displayinfo.js
+++ 104/Petsalon/scripts/displayinfo.js
@@ -1,6 +1,6 @@
 function displayInformation(){
     document.getElementById("salonInformation").innerHTML=`
-        <p> Welcome to ${salon.name}<p>
+        <h2> Welcome to ${salon.name}</h2>
         <p> You can find us at ${salon.adress.street}, ${salon.adress.number}<p>
         <p> Open from ${salon.hours.open} to ${salon.hours.close}<p>
     `
