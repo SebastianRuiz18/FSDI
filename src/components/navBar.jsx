533916b81274931287756c1c12ce12b514a6027c
SebastianRuiz18 <89488090+SebastianRuiz18@users.noreply.github.com>
2022-03-08 17:19:40 -0800

    Delete src directory

--- src/components/navBar.jsx
+++
@@ -1,9 +0,0 @@
-import './navBar.css';
-
-const NavBar = () => {
-    return(<div className="navBar">
-                <h2>This will be the navBar!</h2>
-            </div>);
-};
-
-export default NavBar;
